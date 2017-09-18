92c40ad8611044d3eb0839ca5549ad0796be6190
LI Daobing <lidaobing@gmail.com>
2017-09-18 17:07:06 +0800

    gofmt

--- golunar.go
+++ golunar.go
@@ -1,16 +1,15 @@
 package main
 
-
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path"
 	"strconv"
-	"flag"
 	"strings"
 
 	cc "github.com/lidaobing/chinese_calendar"
-  )
+)
 
 var inverse = flag.Bool("i", false, "from lunar to solar")
 var leap = flag.Bool("l", false, "leap month")
@@ -40,7 +39,7 @@ func printSolarCalendar(year, month, day int, isLeap bool) (err error) {
 	}
 	fmt.Printf("Result: %s\n", t)
 	return nil
-}	
+}
 
 func printChineseCalendar(year, month, day int) (err error) {
 	res, err := cc.FromSolarDate(year, month, day)
@@ -69,7 +68,6 @@ func str2int(year, month, day string) (year2, month2, day2 int, err error) {
 	return
 }
 
-
 func main() {
 	flag.Usage = func() {
 		printHelp()
@@ -88,15 +86,15 @@ func main() {
 	}
 
 	year2, month2, day2, err := str2int(flag.Args()[0], flag.Args()[1], flag.Args()[2])
-	
+
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
 		os.Exit(1)
 	}
 
-	if(*inverse) {
-		err = printSolarCalendar(year2, month2, day2, *leap)		
-	}  else {
+	if *inverse {
+		err = printSolarCalendar(year2, month2, day2, *leap)
+	} else {
 		err = printChineseCalendar(year2, month2, day2)
 	}
 	if err != nil {
